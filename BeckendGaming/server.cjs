f66c851f5689882b65fb228e45f2ddb5735b15af
Alex <alexbiondi1199@gmail.com>
2024-03-27 14:51:02 +0100

    new file:   BeckendGaming/server.cjs

---
+++ BeckendGaming/server.cjs
@@ -0,0 +1,70 @@
+const express = require('express')
+const { Pool } = require('pg');
+
+const app = express();
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'ReactGaming',
+  password: 'root',
+  port: 5432,
+});
+
+pool.connect((err,client,release) => {
+    if(err) {
+        return console.error('Errore di connessione al database',err);
+    }
+    console.log('La connessione ha avuto successo');
+    release();
+})
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.listen(3000, () => {
+    console.log("Server in ascolto sulla porta 3000");
+})
+
+app.get('/', (req, res) => {
+    res.send('Benvenuto su ReactGaming Server!');
+  });
+
+app.post('/signup',(req,res) => {
+    const { name,surname,email,password} = req.body;
+
+    pool.query('INSERT INTO utenti (name,surname,email,password) VALUES ($1, $2, $3, $4)', [name,surname,email,password], (err,result) => {
+        if(err){
+            console.error('Errore inserimento dati',err);
+            res.status(500).json({error: 'Errore nel server'});
+            return;
+        }
+        res.status(201).json({message: "Utente registrato con successo"})
+    })
+})
+
+app.post('/api/login', async (req,res) => {
+    const {email,password} = req.body;
+
+    try {
+        const query = 'SELECT * FROM utenti WHERE email = $1 AND password = $2';
+        const result = await pool.query(query, [email, password]);
+
+        if (result.rows.length === 1) {
+            res.status(200).json({ message: 'Login successful 2' });
+            console.log(res.message )
+        } else {
+            res.status(401).json({ message: 'Invalid email or password' });
+            console.log(res.message )
+        }
+    } catch (error) {
+        console.error('Errore durante il login:', error);
+        res.status(500).json({ error: 'Errore durante il login' });
+    }
+})
