44d420006e244a8cfd5a9e1d9f2945e12fac01df
Alex <alexbiondi1199@gmail.com>
2024-03-27 15:29:09 +0100

    import file

---
+++ reactGaming/src/homePage.jsx
@@ -0,0 +1,11 @@
+import { Card } from "./Card" 
+import { Navbar } from "./Navbar"
+export function HomePage() {
+    
+    return(
+            <div className="homePage">
+                <Navbar />
+                <Card />
+            </div>    
+    )
+}
